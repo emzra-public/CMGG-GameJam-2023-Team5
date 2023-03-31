e657217e6f33100363af580c8ea981125b564e1a
EmalineG <mgayhart0@ivc.edu>
2023-03-31 01:14:34 -0700

    cutscene 1 finished, walls, some aesthetic changes

--- CMGG Game Jam 2023/Assets/Scripts/CutsceneText1.cs
+++ CMGG Game Jam 2023/Assets/Scripts/CutsceneText1.cs
@@ -18,9 +18,13 @@ public class CutsceneText1 : MonoBehaviour
 
     private void Start()
     {
-        dialogueLines.Add("First line of text.");
-        dialogueLines.Add("Second line of text.");
-        dialogueLines.Add("Third line of text.");
+        dialogueLines.Add("I close my eyes and take a deep breath. The air is so crisp and fresh up here in the mountains... When I re-open my eyes, I can’t help but stand still in awe. I’ve never seen so many flowers in one place before... ");
+        dialogueLines.Add("At this point, I hardly mind waiting for her to come back. I know I just met her today, but she seems pretty cool. I wonder if she would want to be my friend…");
+        dialogueLines.Add("“Sooo,” she calls out in a sing-song voice, snapping me back into this reality. “I didn’t find any water buuut I did find this!” Bemused, I watch as she reaches up to place a perfectly woven flower crown onto my head.");
+        dialogueLines.Add("“Where did you-” “Hey, stranger!” she interrupts. Her face twists up into an all-knowing smile and her eyes glitter with intent. “You look pretty cute with that on. Keep it!”");
+        dialogueLines.Add("A blush creeps up onto my face, and I furiously look away. “Y-you know, I’d rather not die of thirst right now so, ummm… let’s keep moving…!”");
+        
+
         // Add more lines as needed.
         StartCoroutine(TypeText(dialogueLines[currentLine]));
         Debug.Log("type text work");
